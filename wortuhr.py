beed59349ec5c9b3fd41dc015facbbf6135d5686
Felix Na <felix.na70@gmail.com>
2022-11-24 18:06:02 +0100

    Zwischenstand (mit Eck-LEDs)

--- wortuhr.py
+++ wortuhr.py
@@ -6,13 +6,20 @@ diffusor_th = 0.4 #- 0.4
 led_dx = 16.6
 led_dy = 18.94
 
-cnt_x = 11
-cnt_y = 10
+cnt_x = 3 # 11
+cnt_y = 3 # 10
 
 size_x = 240 # 2*border + (cnt_x - 1) * led_dx
 size_y = 240 # 2*border + (cnt_y - 1) * led_dy
 
 wall_th = 1.2
+
+cled_dx = led_dx * (cnt_x + 1)
+cled_dy = led_dy * (cnt_y + 1)
+
+grid_height = 10.0
+
+corner_led_dia = 3
 
 letters = iter(list(
     "ESKISTAFÜNF" + 
@@ -26,20 +33,38 @@ letters = iter(list(
     "SIEBENZWÖLF" + 
     "ZEHNEUNKUHR" ))
 
-front = (cq.Workplane("XY")
+# sunken letters
+result = (cq.Workplane("XY")
           .box(size_y, size_x, front_th + diffusor_th)
           .faces(">Z").workplane()
           .rarray(led_dy, led_dx, cnt_y, cnt_x)
-    .eachpoint(lambda loc: (cq.Workplane().workplane(offset=-diffusor_th)
+          .eachpoint(lambda loc: (cq.Workplane().workplane(offset=-diffusor_th)
                  .text(next(letters), 10, -front_th)
                  .rotate((0, 0, 1),(0, 0, 2), 90)
                  .val().moved(loc)), combine='s')
     )
 
-result = (front.faces("<Z").workplane()
+# holes for corner LEDs
+result = (result.rarray(cled_dy, cled_dx, 2, 2)
+          .eachpoint(lambda loc: (cq.Workplane().workplane(offset=-diffusor_th)
+                                  .circle(corner_led_dia/2).extrude(-front_th)
+                                  .val().moved(loc)), combine='s')
+)
+
+
+# boxes for letter LEDs
+result = (result.faces("<Z").workplane()
           .rect(wall_th + cnt_y * led_dy, wall_th + cnt_x * led_dx)
           .rarray(led_dy, led_dx, cnt_y, cnt_x)
-          .rect(led_dy - wall_th, led_dx - wall_th).extrude(10.0)
+          .rect(led_dy - wall_th, led_dx - wall_th)
+          .extrude(grid_height)
+)
+
+# boxes for corner LEDs
+result = (result.rarray(cled_dy, cled_dx, 2, 2)
+          .rect(led_dy - wall_th, led_dx - wall_th) # .extrude(10.0)
+          .rect(led_dy + wall_th, led_dx + wall_th)
+          .extrude(grid_height)
 )
 
 log(f"size_x = {size_x}")
