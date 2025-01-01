f4aa61621ce1d59a38aa33d27280c42e6f4f2c34
Felix Na <felix.na70@gmail.com>
2025-01-01 19:40:51 +0100

    min refactor / rename

--- wortuhr.py
+++ wortuhr.py
@@ -9,7 +9,7 @@ led_dx = 16.6  # x distance of LEDs
 led_dy = 18.94 # Y distance of LEDs
 
 cnt_x = 2
-cnt_y = 3
+cnt_y = 2
 
 wall_th = 1.2
 border = 0.2 + wall_th
@@ -30,7 +30,7 @@ mag_dx = (cnt_x + 1) * led_dx
 mag_dy = (cnt_y + 1) * led_dy
 
 letters = iter(list(
-    "x1234567890A" +
+     "1234567890A" +
 #    "ESKISTAFÜNF" + 
 #    "ZEHNZWANZIG" + 
 #    "DREIVIERTEL" + 
@@ -43,32 +43,32 @@ letters = iter(list(
     "ZEHNEUNKUHR" ))
 
 # Sunken letters and holes
-result = Box(size_y, size_x, front_th + diffusor_th,
+front = Box(size_y, size_x, front_th + diffusor_th,
              align=(Align.CENTER, Align.CENTER, Align.MAX))
-txt_pl = Plane(result.faces().sort_by(Axis.Z).last)
+txt_pl = Plane(front.faces().sort_by(Axis.Z).last)
 
 # Letters
 letter_sk = Sketch() + [
     txt_pl * loc * Text(next(letters), 10, rotation=90)
     for loc in GridLocations(led_dy, led_dx, cnt_y, cnt_x)
 ]
-result -= extrude(letter_sk, -front_th)
+front -= extrude(letter_sk, -front_th)
 
 # Holes for corner LEDs
 holes_sk = Sketch() + [
     txt_pl * loc * Circle(corner_led_dia/2)
     for loc in GridLocations(cled_dy, cled_dx, 2, 2)
 ]
-result -= extrude(letter_sk + holes_sk, -front_th)
+front -= extrude(letter_sk + holes_sk, -front_th)
 
-result = result.mirror(Plane.ZX)
+front = front.mirror(Plane.ZX)
 # boxes for letters
 lboxes_sk = Rectangle(wall_th + cnt_y * led_dy, wall_th + cnt_x * led_dx,
                          align=(Align.CENTER, Align.CENTER, Align.MIN)) - [
     loc * Rectangle(led_dy - wall_th, led_dx - wall_th)
     for loc in GridLocations(led_dy, led_dx, cnt_y, cnt_x)
 ]
-result += extrude(lboxes_sk, grid_height)
+front += extrude(lboxes_sk, grid_height)
 
 # boxes for corner LEDs
 cboxes_sk = Sketch() + [
@@ -76,7 +76,7 @@ cboxes_sk = Sketch() + [
     - loc * Rectangle(led_dy - wall_th, led_dx - wall_th)
     for loc in GridLocations(cled_dy, cled_dx, 2, 2)
 ]
-result += extrude(cboxes_sk, grid_height)
+front += extrude(cboxes_sk, grid_height)
 
 
 # magnet sockets
@@ -90,21 +90,21 @@ mag_sk = Sketch() + [
     loc * Rectangle(mag_dia + 2*wall_th, mag_dia + 2*wall_th)
     for loc in mag_locations
 ]
-result += extrude(mag_sk, grid_height)
+front += extrude(mag_sk, grid_height)
 
 mag_pl = Plane.XY.offset(grid_height)
 mag_sk = Sketch() + [
     mag_pl * loc * Circle(mag_dia/2)
     for loc in mag_locations
 ]
-result -= extrude(mag_sk, -mag_dep)
+front -= extrude(mag_sk, -mag_dep)
 
 
 print(f"size_x = {size_x}") 
 print(f"size_y = {size_y}")
 
-show_object(result)
+show_object(front)
 
 filename = "wortuhr-front"
-export_step(result, f"{filename}.step")
-export_stl(result, f"{filename}.stl")
+export_step(front, f"{filename}.step")
+export_stl(front, f"{filename}.stl")
