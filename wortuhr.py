ad14e1d55ac7af5b962b730f680ec66401e0254e
Felix Na <felix.na70@gmail.com>
2025-01-01 17:43:18 +0100

    added magnet holders

--- wortuhr.py
+++ wortuhr.py
@@ -8,15 +8,14 @@ diffusor_th = 0.4 # diffusor thickness
 led_dx = 16.6  # x distance of LEDs
 led_dy = 18.94 # Y distance of LEDs
 
-cnt_x = 11
-cnt_y = 10
-
-border = 42
-
-size_x = 2*border + (cnt_x - 1) * led_dx
-size_y = 2*border + (cnt_y - 1) * led_dy
+cnt_x = 2
+cnt_y = 3
 
 wall_th = 1.2
+border = 0.2 + wall_th
+
+size_x = 2*border + (cnt_x + 2) * led_dx + wall_th
+size_y = 2*border + (cnt_y + 2) * led_dy + wall_th
 
 cled_dx = led_dx * (cnt_x + 1)
 cled_dy = led_dy * (cnt_y + 1)
@@ -25,22 +24,29 @@ grid_height = 10.0
 
 corner_led_dia = 3
 
+mag_dia = 8.2
+mag_dep = 3.8
+mag_dx = (cnt_x + 1) * led_dx
+mag_dy = (cnt_y + 1) * led_dy
+
 letters = iter(list(
-    "ESKISTAFÜNF" + 
-    "ZEHNZWANZIG" + 
-    "DREIVIERTEL" + 
-    "VORFUNKNACH" + 
-    "HALBAELFÜNF" + 
-    "EINSDCWZWEI" + 
-    "DREIAUJVIER" + 
-    "SECHSNLACHT" + 
-    "SIEBENZWÖLF" + 
+    "x1234567890A" +
+#    "ESKISTAFÜNF" + 
+#    "ZEHNZWANZIG" + 
+#    "DREIVIERTEL" + 
+#    "VORFUNKNACH" + 
+#    "HALBAELFÜNF" + 
+#    "EINSDCWZWEI" + 
+#    "DREIAUJVIER" + 
+#    "SECHSNLACHT" + 
+#    "SIEBENZWÖLF" + 
     "ZEHNEUNKUHR" ))
 
 # Sunken letters and holes
 result = Box(size_y, size_x, front_th + diffusor_th,
              align=(Align.CENTER, Align.CENTER, Align.MAX))
 txt_pl = Plane(result.faces().sort_by(Axis.Z).last)
+
 # Letters
 letter_sk = Sketch() + [
     txt_pl * loc * Text(next(letters), 10, rotation=90)
@@ -72,7 +78,33 @@ cboxes_sk = Sketch() + [
 ]
 result += extrude(cboxes_sk, grid_height)
 
+
+# magnet sockets
+mag_locations = Locations(
+    (-mag_dy/2 , 0),
+    (+mag_dy/2 , 0),
+    (0, -mag_dx/2),
+    (0, +mag_dx/2)
+)
+mag_sk = Sketch() + [
+    loc * Rectangle(mag_dia + 2*wall_th, mag_dia + 2*wall_th)
+    for loc in mag_locations
+]
+result += extrude(mag_sk, grid_height)
+
+mag_pl = Plane.XY.offset(grid_height)
+mag_sk = Sketch() + [
+    mag_pl * loc * Circle(mag_dia/2)
+    for loc in mag_locations
+]
+result -= extrude(mag_sk, -mag_dep)
+
+
 print(f"size_x = {size_x}") 
 print(f"size_y = {size_y}")
 
 show_object(result)
+
+filename = "wortuhr-front"
+export_step(result, f"{filename}.step")
+export_stl(result, f"{filename}.stl")
