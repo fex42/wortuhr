2a01bfea25cf383c45cca95c731703c45e11f43e
Felix Na <felix.na70@gmail.com>
2025-01-12 12:08:31 +0100

    fix foot orientation and size

--- foot.py
+++ foot.py
@@ -8,7 +8,7 @@ foot_l = 25.0 # foot length
 foot_th = 3.5 # foot thickness
 screw_h = 39.0 # screw height
 stand_h = 44 # stand height
-clock_th = 19.0 + tol # clock thichness (and tolerance)
+clock_th = 19.5 + tol # clock thichness (and tolerance)
 nups_h = 2.0
 
 width = 16.0
@@ -32,11 +32,11 @@ pts = [
 
 ln = Polyline(pts)
 
-foot_sk = make_face(Plane.YZ * ln)
+foot_sk = make_face(Plane.YX * ln)
 foot = extrude(foot_sk, -width).clean()
 
-hole_sk = Plane.XZ * Pos(width/2, screw_h) * Circle(hole_dia/2)
-foot -= extrude(hole_sk,-10)
+hole_sk = Plane.ZX * Pos(-width/2, screw_h) * Circle(hole_dia/2)
+foot -= extrude(hole_sk,10)
 
 show(foot)
 
